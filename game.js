9ed045c9df0a8b8e7fe4cba5eef9a8bcbd3a7998
samboxft <samGreenLite@gmail.com>
2021-01-03 21:19:53 +0200

    fdsf

--- game.js
+++ game.js
@@ -18,78 +18,79 @@ document.getElementById("start").addEventListener("click", handler);
 
 // handler function
 function handler(e) {
-    // remove this handler
-    e.target.removeEventListener(e.type, arguments.callee);
+	// remove this handler
+	e.target.removeEventListener(e.type, arguments.callee);
     this.remove();
-    $("#level-title").text("Level " + level)
-    nextSequence();
-    started = true;
+    alert("You'll only see this once!");
+         $("#level-title").text("Level " + level)
+        nextSequence();
+        started = true;
 }
 
 
-$(".btn").click(function () {
-    // console.log(event);
-    let userChosenColor = $(this).attr("id");
-    userClickedPattern.push(userChosenColor);
-    let lastColor = userClickedPattern.length - 1;
-    playSound(userChosenColor);
-    animatePress(userChosenColor);
-    checkAnswer(userClickedPattern.length - 1)
-});
+    $(".btn").click(function () {
+        // console.log(event);
+        let userChosenColor = $(this).attr("id");
+        userClickedPattern.push(userChosenColor);
+        let lastColor = userClickedPattern.length - 1;
+        playSound(userChosenColor);
+        animatePress(userChosenColor);
+        checkAnswer(userClickedPattern.length - 1)
+    });
 
 
 
-function checkAnswer(currentLevel) {
+    function checkAnswer(currentLevel) {
 
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-        if (userClickedPattern.length === gamePattern.length) {
-            setTimeout(function () { nextSequence() }, 1000);
+        if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+            if (userClickedPattern.length === gamePattern.length) {
+                setTimeout(function () { nextSequence() }, 1000);
+            }
+        } else {
+            playSound("wrong");
+            $("body").addClass("game-over");
+            $("h1").text("Game Over");
+            setTimeout(() => {
+                $("body").removeClass("game-over");
+            }, 500);
+            startOver();
+
         }
-    } else {
-        playSound("wrong");
-        $("body").addClass("game-over");
-        $("h1").text("Game Over");
-        setTimeout(() => {
-            $("body").removeClass("game-over");
-        }, 500);
-        startOver();
-
     }
-}
 
-function nextSequence() {
-    userClickedPattern = [];
-    level++;
-    $("#level-title").text(`Level   ${level}`);
-    let randomNumber = Math.floor(Math.random() * 4);
-    let randomChosenColor = buttonColors[randomNumber];
-    let chosenButton = $("#" + randomChosenColor);
-    chosenButton.fadeOut(100).fadeIn(100);
-    gamePattern.push(randomChosenColor);
-    playSound(randomChosenColor);
-};
-
+    function nextSequence() {
+        userClickedPattern = [];
+        level++;
+        $("#level-title").text(`Level   ${level}`);
+        let randomNumber = Math.floor(Math.random() * 4);
+        let randomChosenColor = buttonColors[randomNumber];
+        let chosenButton = $("#" + randomChosenColor);
+        chosenButton.fadeOut(100).fadeIn(100);
+        gamePattern.push(randomChosenColor);
+        playSound(randomChosenColor);
+    };
 
 
-function animatePress(currentColor) {
-    $('#' + currentColor).addClass("pressed");
-    setTimeout(() => {
-        $("#" + currentColor).removeClass("pressed");
-    }, 100);
-};
-function playSound(name) {
-    let audio = new Audio("sounds/" + name + ".mp3");
-    audio.play();
-};
 
-function startOver() {
-    level = 0;
-    gamePattern = [];
-    started = true;
-    setTimeout(() => {
-        window.location.reload()
-    }, 2000);
-}
+    function animatePress(currentColor) {
+        $('#' + currentColor).addClass("pressed");
+        setTimeout(() => {
+            $("#" + currentColor).removeClass("pressed");
+        }, 100);
+    };
+    function playSound(name) {
+        let audio = new Audio("sounds/" + name + ".mp3");
+        audio.play();
+    };
+
+    function startOver() {
+        level = 0;
+        gamePattern = [];
+        started = true;
+        setTimeout(() => {
+            window.location.reload()
+        }, 2000);
+    }
 
 
 
